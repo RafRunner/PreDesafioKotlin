4324f2eb6445b2fd4b2341a5d3ace94191a39873
Rafael Nunes <rafaelns.br@gmail.com>
2020-09-23 15:49:23 -0300

    Criando a classe Item

--- src/br/com/digitalhouse/dominio/Item.kt
+++ src/br/com/digitalhouse/dominio/Item.kt
@@ -1,4 +1,30 @@
 package br.com.digitalhouse.dominio
 
-class Item {
+import java.lang.Exception
+import java.math.BigDecimal
+import java.math.BigInteger
+
+class Item(val codigo: String, preco: BigDecimal, val descricao: String, qtdEstoque: Int) {
+
+    var preco: BigDecimal = BigDecimal.ZERO
+        set(value) {
+            if (value.compareTo(BigDecimal.ZERO) <= 0) {
+                throw Exception("Valor inválido, deve ser maior que zero")
+            }
+
+            field = value
+        }
+    var qtdEstoque: Int = 0
+        set(value) {
+            if (value <= 0) {
+                throw Exception("Quantidade inválida, deve ser maior que zero")
+            }
+
+            field = value
+        }
+
+    init {
+        this.preco = preco
+        this.qtdEstoque = qtdEstoque
+    }
 }
